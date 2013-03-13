92a232efc9d61fc2ea85df1000fdbdb2a8a7a1fb
Robert (Shared Mac) Melton <rmelton@inky.com>
2013-03-13 17:57:50 -0400

    ugh, better now

--- setup.zsh
+++ setup.zsh
@@ -1,15 +1,14 @@
 #!/bin/zsh
 
-rm ~/.tmux.conf
-ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
-
-rm ~/.zshrc
-ln -s ~/.dotfiles/.zshrc ~/.zshrc
-
 rm -rf ~/.vim
+rm ~/.dotfiles/vim-files
+rm ~/.tmux.conf
 rm ~/.vimrc
+rm ~/.zshrc
 
-rm -rf vim-files
 git clone git://github.com/robertmeta/vim-files.git
+
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/vim-files ~/.vim
 ln -s ~/.dotfiles/vim-files/.vimrc ~/.vimrc
