8bfde2224e897782bcbe2b4f9edd2ebf2d9259d4
Robert Melton <robert@robertmelton.com>
2013-06-01 03:39:28 -0400

    Added in git settings

--- setup.zsh
+++ setup.zsh
@@ -16,3 +16,30 @@ zsh ~/.zshrc
 tmux source-file ~/.tmux.conf
 
 cd ~/.vim && git submodule update --init
+
+git config --global color.ui true
+git config --replace-all --global format.pretty "short"
+git config --global alias.lg "log --color --decorate --graph --oneline --abbrev-commit"
+git config --global alias.graph "log --graph --all --pretty=format:'%Cred%h%Creset - %Cgreen(%cr)%Creset %s%C(yellow)%d%Creset' --abbrev-commit --date=relative"
+git config --global alias.wip "commit -a -m WIP"
+git config --global alias.staged 'diff --cached'
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.a add
+git config --global alias.snapshot '!git stash save "snapshot: $(date)" && git stash apply "stash@{0}"'
+git config --global diff.algorithm patience
+git config --global help.autocorrect 1
+git config --global push.default tracking
+git config --global --bool pull.rebase false
+git config --global advice.pushNonFastForward true
+git config --global advice.statusHints true
+git config --global diff.renames copies
+git config --global core.editor vim
+git config --global merge.tool vimdiff
+git config --global core.whitespace trailing-space,space-before-tab
+git config --global diff.mnemonicprefix true
+git config --global branch.autosetupmerge true
+git config --global merge.stat true
+git config --global rerere.enabled true
