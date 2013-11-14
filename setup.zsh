91c14f4d7556d22c2b8e3ab973c970c2748195c3
Robert Melton <robert@robertmelton.com>
2013-11-14 11:38:44 -0500

    Custom ctags supporting go

--- setup.zsh
+++ setup.zsh
@@ -5,6 +5,7 @@ rm -rf ~/.emacs.d
 rm ~/.tmux.conf
 rm ~/.vimrc
 rm ~/.zshrc
+rm ~/.ctags
 rm ~/.gitconfig
 
 git clone git://github.com/robertmeta/vim-files.git ~/.dotfiles/vim-files
@@ -12,6 +13,7 @@ git clone git://github.com/robertmeta/emacs-files.git ~/.dotfiles/emacs-files
 
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
+ln -s ~/.dotfiles/.ctags ~/.ctags
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vim-files ~/.vim
 ln -s ~/.dotfiles/vim-files/.vimrc ~/.vimrc
