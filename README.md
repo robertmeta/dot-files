6271e7ea675fb80a645c0df0c5bc37b53fcdc65b
Robert Melton <robert@robertmelton.com>
2015-04-03 07:53:12 -0400

    WIP

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-rm -rf ~/.dotfiles; git clone https://github.com/robertmeta/dotfiles.git ~/.dotfiles && zsh ~/.dotfiles/setup.zsh
+Linux: rm -rf ~/.dotfiles; git clone https://github.com/robertmeta/dotfiles.git ~/.dotfiles && zsh ~/.dotfiles/setup.zsh
+
+Windows (Admin Prompt): rm -rf "%HOME%\.dotfiles"; git clone https://github.com/robertmeta/dotfiles.git "%HOME%/.dotfiles" && zsh "%HOME%/.dotfiles/setup.bat"
---
+++ setup.bat
@@ -0,0 +1,11 @@
+rm -rf "%HOME%\vimfiles"
+rm "%HOME%\.ctags"
+rm "%HOME%\.gitconfig"
+rm "%HOME%\.agignore"
+
+git clone "https://github.com/robertmeta/vimfiles.git" "%HOME%\.dotfiles\vimfiles"
+
+mklink "%HOME%\.ctags" "%HOME%\.dotfiles\.ctags"
+mklink "%HOME%\.gitconfig" "%HOME%\.dotfiles\.gitconfig"
+mklink "%HOME%\.agignore" "%HOME%\.dotfiles\.agignore"
+mklink /D "%HOME%\vimfiles" "%HOME%\.dotfiles\vimfiles" 
