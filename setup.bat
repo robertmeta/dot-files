6c59ad908fb0b11a3a0235005fe56d048b3e34d1
Robert Melton <robert@robertmelton.com>
2015-05-20 03:23:06 -0500

    WIP

--- setup.bat
+++ setup.bat
@@ -1,11 +1,13 @@
 rm -rf "%HOME%\vimfiles"
 rm "%HOME%\.ctags"
+rm "%HOME%\.gitconfig_global"
 rm "%HOME%\.gitconfig"
 rm "%HOME%\.agignore"
 
 git clone "https://github.com/robertmeta/vimfiles.git" "%HOME%\.dotfiles\vimfiles"
 
 mklink "%HOME%\.ctags" "%HOME%\.dotfiles\.ctags"
+mklink "%HOME%\.gitconfig_global" "%HOME%\.dotfiles\.gitconfig_global"
 mklink "%HOME%\.gitconfig" "%HOME%\.dotfiles\.gitconfig"
 mklink "%HOME%\.agignore" "%HOME%\.dotfiles\.agignore"
 mklink /D "%HOME%\vimfiles" "%HOME%\.dotfiles\vimfiles" 
--- setup.zsh
+++ setup.zsh
@@ -6,6 +6,7 @@ rm ~/.tmux.conf
 rm ~/.vimrc
 rm ~/.zshrc
 rm ~/.ctags
+rm ~/.gitignore_global
 rm ~/.gitconfig
 rm ~/.agignore
 
@@ -14,6 +15,7 @@ git clone git://github.com/robertmeta/vimfiles.git ~/.dotfiles/vimfiles
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.zshrc ~/.zshrc
 ln -s ~/.dotfiles/.ctags ~/.ctags
+ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vimfiles ~/.vim
 ln -s ~/.dotfiles/vimfiles ~/.nvim
