84bb5e3708e0f5a4d9834f27769fcdc222d689c7
Robert Melton <robert@robertmelton.com>
2015-08-10 12:29:27 -0500

    WIP

---
+++ setup.ps1
@@ -0,0 +1,19 @@
+cd $env:USERPROFILE
+mkdir "Documents\WindowsPowerShell"
+rm -recurse -force "vimfiles"
+rm ".ctags"
+rm ".gitignore_global"
+rm ".gitconfig"
+rm ".agignore"
+rm ".dotfiles\Microsoft.PowerShell_profile.ps1"
+
+git clone "https://github.com/robertmeta/vimfiles.git" ".dotfiles\vimfiles"
+
+cmd /c mklink "%USERPROFILE%\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1" "%USERPROFILE%\.dotfiles\Microsoft.PowerShell_profile.ps1"
+cmd /c mklink "%USERPROFILE%\.ctags" "%USERPROFILE%\.dotfiles\.ctags"
+cmd /c mklink "%USERPROFILE%\.gitignore_global" "%USERPROFILE%\.dotfiles\.gitignore_global"
+cmd /c mklink "%USERPROFILE%\.gitconfig" "%USERPROFILE%\.dotfiles\.gitconfig"
+cmd /c mklink "%USERPROFILE%\.agignore" "%USERPROFILE%\.dotfiles\.agignore"
+cmd /c mklink /D "%USERPROFILE%\vimfiles" "%USERPROFILE%\.dotfiles\vimfiles"
+
+cat ".gitconfig" | %{$_ -replace "cache --timeout=64800","wincred"}
