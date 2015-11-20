2ca1a87afdd066d31ca9c051b81e8309c0fd5d1f
Robert Melton <robert@robertmelton.com>
2015-11-19 20:51:18 -0600

    WIP

--- setup.zsh
+++ setup.zsh
@@ -11,6 +11,9 @@ rm ~/.gitignore_global
 rm ~/.gitconfig
 rm ~/.agignore
 
+mkdir ~/.dotfiles
+mkdir ~/.config
+mkdir ~/.config/nvim
 git clone git://github.com/robertmeta/vimfiles.git ~/.dotfiles/vimfiles
 
 ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
@@ -21,6 +24,7 @@ ln -s ~/.dotfiles/.gitignore_global ~/.gitignore_global
 ln -s ~/.dotfiles/.gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/vimfiles ~/.vim
 ln -s ~/.dotfiles/vimfiles ~/.nvim
+ln -s ~/.dotfiles/vimfiles/vimrc ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/.agignore ~/.agignore
 
 source ~/.zshrc
