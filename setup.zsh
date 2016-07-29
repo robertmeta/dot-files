4c1d685ba5e85a2d0968007168624c3ac660f0c2
Robert Melton <robert@robertmelton.com>
2016-07-29 17:04:47 +0000

    Does ssh keys and config now

--- setup.zsh
+++ setup.zsh
@@ -11,9 +11,11 @@ rm ~/.ctags
 rm ~/.gitignore_global
 rm ~/.gitconfig
 rm ~/.agignore
+rm ~/.ssh/config
 
 mkdir ~/.dotfiles
 mkdir ~/.config
+mkdir ~/.ssh
 
 git clone git://github.com/robertmeta/vimfiles.git ~/.dotfiles/vimfiles
 
@@ -27,6 +29,11 @@ ln -s ~/.dotfiles/vimfiles ~/.vim
 ln -s ~/.dotfiles/vimfiles ~/.config/nvim
 ln -s ~/.config/nvim/vimrc ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/.agignore ~/.agignore
+ln -s ~/.dotfiles/sshconfig ~/.ssh/config
+
+cat ~/.dotfiles/id_rsa.pub >> ~/.ssh/authorized_keys
+cat ~/.ssh/authorized_keys | uniq > ~/.ssh/authorized_keys.tmp
+mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys
 
 source ~/.zshrc
 tmux source-file ~/.tmux.conf
