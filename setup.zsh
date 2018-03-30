6f25f2567b94a1a9814fc2fb32d1e0c7f1aa977e
Robert Melton <robert@robertmelton.com>
2018-03-30 05:12:45 +0000

    prompt

--- setup.zsh
+++ setup.zsh
@@ -43,25 +43,26 @@ source ~/.zshrc
 tmux source-file ~/.tmux.conf
 
 if [ -x "$(command -v go)" ]; then
-    read "reply?Update go support apps? " -r
+    read "reply?Update go support apps? "
     if [[ "$reply" =~ ^[Yy]$ ]]; then
-        go get -u -v github.com/alecthomas/gometalinter
-        go get -u -v github.com/davidrjenni/reftools/cmd/fillstruct
-        go get -u -v github.com/derekparker/delve/cmd/dlv
-        go get -u -v github.com/dominikh/go-tools/cmd/keyify
-        go get -u -v github.com/fatih/gomodifytags
-        go get -u -v github.com/fatih/motion
-        go get -u -v github.com/golang/lint/golint
-        go get -u -v github.com/josharian/impl
-        go get -u -v github.com/jstemmer/gotags
-        go get -u -v github.com/kisielk/errcheck
-        go get -u -v github.com/klauspost/asmfmt/cmd/asmfmt
-        go get -u -v github.com/nsf/gocode
-        go get -u -v github.com/rogpeppe/godef
-        go get -u -v github.com/zmb3/gogetdoc
-        go get -u -v golang.org/x/tools/cmd/goimports
-        go get -u -v golang.org/x/tools/cmd/gorename
-        go get -u -v golang.org/x/tools/cmd/guru
+        go get -u github.com/monochromegane/the_platinum_searcher/...
+        go get -u github.com/alecthomas/gometalinter
+        go get -u github.com/davidrjenni/reftools/cmd/fillstruct
+        go get -u github.com/derekparker/delve/cmd/dlv
+        go get -u github.com/dominikh/go-tools/cmd/keyify
+        go get -u github.com/fatih/gomodifytags
+        go get -u github.com/fatih/motion
+        go get -u github.com/golang/lint/golint
+        go get -u github.com/josharian/impl
+        go get -u github.com/jstemmer/gotags
+        go get -u github.com/kisielk/errcheck
+        go get -u github.com/klauspost/asmfmt/cmd/asmfmt
+        go get -u github.com/nsf/gocode
+        go get -u github.com/rogpeppe/godef
+        go get -u github.com/zmb3/gogetdoc
+        go get -u golang.org/x/tools/cmd/goimports
+        go get -u golang.org/x/tools/cmd/gorename
+        go get -u golang.org/x/tools/cmd/guru
     fi
 fi
 
