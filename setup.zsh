542b6fbbc5929bf2f3d05e8fcc80bf73feaf9aa9
Robert Melton <robert@robertmelton.com>
2019-11-15 02:17:32 -0500

    kars for kids

--- setup.zsh
+++ setup.zsh
@@ -111,6 +111,8 @@ if [ -x "$(command -v cargo)" ]; then
     if [[ "$reply" =~ ^[Yy]$ ]]; then
         cargo install ripgrep --force
         echo "Installed ripgrep"
+        cargo install skim --force
+        echo "Installed skim"
     fi
 fi
 
