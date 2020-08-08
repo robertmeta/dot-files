f3158a92762148a6b5f8f8cdfd940d13d6ce5c08
Robert Melton <robert@robertmelton.com>
2020-08-08 13:43:04 -0400

    Revert "right direction"
    This reverts commit 1887b05572cd54eb0dddff5232b42f4dfb27b19f.

--- setup.zsh
+++ setup.zsh
@@ -44,7 +44,8 @@ ln -s ~/.dotfiles/pt ~/.config/pt
 ln -s ~/.config/nvim/vimrc ~/.config/nvim/init.vim
 ln -s ~/.dotfiles/.agignore ~/.agignore
 ln -s ~/.dotfiles/sshconfig ~/.ssh/config
-ln -s ~/.local/share/broot/launcher/bash/1 ~/.config/broot/launcher/bash/br
+ln -s ~/.config/broot/launcher/bash/br ~/.local/share/broot/launcher/bash/1
+
 cat ~/.dotfiles/id_rsa.pub >> ~/.ssh/authorized_keys
 cat ~/.ssh/authorized_keys | uniq > ~/.ssh/authorized_keys.tmp
 mv ~/.ssh/authorized_keys.tmp ~/.ssh/authorized_keys
